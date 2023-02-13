e1d5041ce980ad2ac1c1a633610b76630e131255
ashutoshumar <viraj28300@gmail.com>
2023-02-13 16:55:36 +0530

    hii you

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : 'https://bloggerashu.herokuapp.com/'
-   // baseURL:'http://localhost:5000/'
+   // baseURL : 'https://bloggerashu.herokuapp.com/'
+   // baseURL:'http://localhost:5000/'  
+    //baseURL:'https://blogserver-7kvi.onrender.com'
+    baseURL:'https://tame-ruby-duckling-gown.cyclic.app'
 })
